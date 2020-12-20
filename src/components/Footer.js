892a312fa3831fe726d6a4522773017a902f832b
yancha1989 <34509508+yancha1989@users.noreply.github.com>
2020-12-21 02:25:48 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,22 +7,22 @@ function Footer(){
             <div className="content__container">
                 <div className="footer__frame-wrapper">
                     <div>
-                        <img className="footer__frame-copyright" src="../img/copyright.svg" alt />
+                        <img className="footer__frame-copyright" src="../img/copyright.svg"/>
                     </div>
                     <div className="footer__frame-socmedia">
                         <div>
                             <a className="footer__frame-item" href="https://www.instagram.com/skilldriveorg/?hl=ru" aria-label="You can find vk profile here">
-                                <img src="../img/vk_icon.svg" alt />
+                                <img src="../img/vk_icon.svg"/>
                             </a>
                         </div>
                         <div>
                             <a className="footer__frame-item" href="https://www.instagram.com/skilldriveorg/?hl=ru" aria-label="You can find instagram profile here">
-                                <img src="../img/instagram_icon.svg" alt />
+                                <img src="../img/instagram_icon.svg"/>
                             </a>
                         </div>
                         <div>
                             <a className="footer__frame-item" href="https://www.instagram.com/skilldriveorg/?hl=ru" aria-label="You can find facebook profile here">
-                                <img src="../img/facebook_icon.svg" alt />
+                                <img src="../img/facebook_icon.svg"/>
                             </a>
                         </div>
                     </div>
